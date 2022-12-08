fdbd829624cb4f0330b8db6292e2a687c6f634e7
Karera-o <olivierkarera2020@gmail.com>
2022-12-09 01:06:33 +0200

    wip

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import user from '../Resources/User.png'
+import email from '../Resources/Atsign.png'
+import message from '../Resources/Envelope.png'
 
 const Contact = () => {
   return (
@@ -7,6 +10,10 @@ const Contact = () => {
         <div className='flex gap-2 justify-around flex-col sm:flex-row'>
             <ContactCard1/>
             <ContactCar2 />
+        </div>
+        <h1 className='text-2xl ml-3 my-10'>Get in touch</h1>
+        <div>
+            <FormContact />
         </div>
     </div>
   )
@@ -32,5 +39,30 @@ const ContactCar2 = ()=>{
                 <p className='flex justify-between'>Github: <span>https://github.com/Karera-o</span></p>
                 <p className='flex justify-between'>Twitter: <span>https://twitter.com/OlivierKarera1</span></p>
         </div>
+    )
+}
+
+
+
+
+const FormContact = ()=>{
+    return(
+        <form className='bg-[#2B2B37] p-2 sm:py-3 sm:mx-4 sm:px-4 gap-y-2 flex flex-col'>
+            <div className='flex'>
+                <label htmlFor="" className='bg-[#20202A] p-3'><img src={user} alt="" /></label>
+                <input type="text" placeholder='Name' className='bg-[#242430] w-full text-sm pl-2'/>
+            </div>
+            <div className='flex'>
+                <label htmlFor="" className='bg-[#20202A] p-3'><img src={email} alt="" /></label>
+                <input type="email" placeholder='Email' className='bg-[#242430] w-full text-sm pl-2'/>
+            </div>
+            <div className='flex'>
+                <label htmlFor="" className='bg-[#20202A] p-3'><img src={message} alt="" /></label>
+                
+                <textarea name="" id="" cols="30" rows="10" placeholder='Message' className='bg-[#242430] w-full text-sm pl-2 pt-3'></textarea>
+            </div>
+
+            <button className='bg-[#FFC107] text-black p-2 sm:w-2/6 lg:w-2/6 xl:w-1/6'>Send Message</button>
+        </form>
     )
 }
