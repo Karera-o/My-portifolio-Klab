20ccadcdf21f7f5efd5697c6f8517278b93fccf6
Karera-o <olivierkarera2020@gmail.com>
2022-12-08 14:17:45 +0200

    wip

--- src/components/homeSide.jsx
+++ src/components/homeSide.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+// import bg from '../Resources/bg.png'
 
 const HomeSide = () => {
   return (
-    <div>homeSide</div>
+    <div className='w-full bg-slate-500'>
+      <div className='w-full h-[50vh] bg-slate-800'>
+        <div className='h-full w-full bg-bg bg-cover bg-center bg-no-repeat flex items-center justify-center '>
+        <div className='h-5/6 w-full mx-4 bg-bg brightness-[30%] bg-cover bg-center bg-no-repeat '>
+            
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
