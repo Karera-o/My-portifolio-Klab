5675d40c04d167d614efa04d970bcd8a3fdfda87
Karera-o <olivierkarera2020@gmail.com>
2022-12-09 14:32:50 +0200

    wip

--- src/components/homeSide.jsx
+++ src/components/homeSide.jsx
@@ -3,22 +3,24 @@ import Contact from './contact';
 import Footer from './footer';
 import ServiceCard from './serviceCard';
 // import bg from '../Resources/bg.png'
+import profile from '../Resources/profile.jpg'
 
 const HomeSide = () => {
   return (
     <div className='w-full bg-slate-500 scroll-auto'>
-      <div className='w-full h-[60vh] bg-slate-800'>
+      <div className='w-full h-[60vh] bg-slate-800 '>
         <div className='h-full w-full bg-bg bg-cover bg-center   bg-no-repeat flex flex-col items-center justify-end '>
-            <div className='h-5/6 w-[95%] mx-4 bg-bg bg-cover contrast-50 bg-center bg-no-repeat '>
+            <div className='h-5/6 w-[95%] mx-4 bg-bg bg-cover contrast-50 bg-center bg-no-repeat flex  items-center justify-around gap-x-5'>
             {/* brightness-[30%] */}
-            <div>
+            <div className='flex flex-col w-2/5'>
             <h1 className='text-white'>Discover my Amazing Art Space! </h1>
 
             <code> <code>I build web applications </code></code>
             
-            <button>Explore Now</button>
+            <button className='bg-[#FFC107] text-black p-2 w-1/2'>Explore Now</button>
         </div>
         {/* <img src="./PHOTO 3.jpg" alt="profile"/> */}
+        <img src={profile} alt=""  className='w-30 h-40 self-end'/>
             </div>
             
         </div>
