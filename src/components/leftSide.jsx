d0692b4cfef6ae4a0d37e19750dbbe3e5bbcefa5
Karera-o <olivierkarera2020@gmail.com>
2022-12-12 14:01:26 +0200

    wip

--- src/components/leftSide.jsx
+++ src/components/leftSide.jsx
@@ -5,6 +5,8 @@ import web from '../Resources/Geography.png'
 import github from '../Resources/GitHub.png'
 import twitter from '../Resources/Twitter.png'
 import { Progress } from 'antd';
+import {BsCheck} from 'react-icons/bs'
+import './all.css'
 
 const LeftSide = () => {
   const percent = [
@@ -16,16 +18,18 @@ const LeftSide = () => {
 ]
 
 const sites = [linkedin,web,github,twitter]
+const skills = [{language:'Nodejs',percent: 90},{language:'ReactJs',percent: 95},{language:'MongoDB',percent: 70},{language:'Postgresql',percent: 75},{language:'REST API',percent: 100},{language:'Jquery',percent: 50},{language:'Tailwindcss',percent: 95},{language:'Bootstrap',percent: 60}]
+const coding = [{language:'html',percent: 90},{language:'css',percent: 80},{language:'Javascript',percent: 80},{language:'Python',percent: 70},{language:'Java',percent: 60},{language:'C',percent: 65}]
   return (
     <div className=' h-full bg-[#20202A] w-full flex flex-col'>
-      <div className='bg-[#242430] text-[#868688] flex flex-col items-center justify-center py-4'>
+      <div className='bg-[#242430] text-[#868688] flex flex-col items-center justify-center py-4 h-30'>
             <img src={profile} alt="" width="1200" className='rounded-full h-[5rem] w-[5rem]'/>
             <h1 className='text-white text-lg font-bold'>Karera Olivier</h1>
             <p className='text-[#868688]'>Full-Stack Developer </p>
             <p className='text-[#868688]'>UI/UX Designer</p>
         </div>
         
-        <div className='flex-1'>
+        <div className='overflow-y-auto no-scrollbar w-full h-96 pb-20'>
 
         <section className='flex flex-col gap-y-1 mt-1'>
                 <p className='flex justify-between mx-3'>Residence <span className='text-[#868688]' >Rwanda</span></p>
@@ -48,14 +52,43 @@ const sites = [linkedin,web,github,twitter]
                 </div>
           </section>
         </div>
+        <div>
+        <hr className='mx-3'/>
+        <h1 className='text-xl ml-3 '>Coding</h1>
+        
+          {coding.map((value)=>{
+            return(
+              <div className='px-4 flex flex-col relative'>
+                <p className='flex justify-between text-xs  '>{value.language} <span>{value.percent}%</span></p>
+        <progress className='progress w-4/5 absolute top-5 ' value={value.percent} max={100} ></progress>
+        </div>
+            )
+          })}
+        
+        </div>
+        <div>
+        <hr className='mx-3'/>
+        <h1 className='text-xl ml-3 '>Skills</h1>
+        
+          {skills.map((value)=>{
+            return(
+              <div className='px-4 flex gap-x-2'>
+                <span className='text-[#FFC107]'><BsCheck color='yellow'/></span>
+                <p className='flex justify-between text-xs text-[] '>{value.language} </p>
+  
+        </div>
+            )
+          })}
+        
+        </div>
         </div>
         
-        <div className='flex justify-between gap-x-2 px-16 bg-[#24242E] py-3 '>
+        <footer className='flex justify-between gap-x-2 px-16 bg-[#24242E] py-3 h-12 fixed bottom-2 w-[16.4rem]'>
           {sites.map(value =>{
             return <img src={value} alt='' className='h-5'/>
           })}
             
-        </div>
+        </footer>
    
     </div>
   )
